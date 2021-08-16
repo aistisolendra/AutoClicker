cfcb6b79a366377fc8224a016d18944d56899887
Aistis Olendra <aistis3211@gmail.com>
2021-08-16 20:45:00 +0300

    Extract MousePoint struct from MouseManager

---
+++ AutoClicker/Structs/MousePoint.cs
@@ -0,0 +1,24 @@
+﻿using System.Runtime.InteropServices;
+using AutoClicker.Models;
+
+namespace AutoClicker.Structs
+{
+    [StructLayout(LayoutKind.Sequential)]
+    public struct MousePoint
+    {
+        public int X;
+        public int Y;
+
+        public MousePoint(int x, int y)
+        {
+            X = x;
+            Y = y;
+        }
+
+        public MousePoint(BasicClicker clicker)
+        {
+            X = clicker.ClickPosition.MousePositionToClick.X;
+            Y = clicker.ClickPosition.MousePositionToClick.Y;
+        }
+    }
+}
